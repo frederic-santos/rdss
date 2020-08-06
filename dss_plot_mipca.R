42b51c20dda7f6a5f860cbdb92d42a7709404db8
f-santos <frederic.santos@u-bordeaux.fr>
2020-08-06 11:09:28 +0200

    Ajout de la fonction plot_mipca

---
+++ dss_plot_mipca.R
@@ -0,0 +1,13 @@
+dss_plot_mipca <- function(midata) {
+### midata: object return by MIPCA()
+
+    if (is.null(midata)) {
+        return()
+    } else {
+        par(mfrow = c(1, 2))
+        plot.MIPCA(x = midata, choice = "ind.supp",
+                   graph.type = "classic", new.plot = FALSE)
+        plot.MIPCA(x = midata, choice = "var",
+                   graph.type = "classic", new.plot = FALSE)
+    }
+}
