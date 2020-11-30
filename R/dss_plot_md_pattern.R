5812d54bdf1d891e6c08734a6516f1c27eb6babe
f-santos <frederic.santos@u-bordeaux.fr>
2020-11-30 09:37:30 +0100

    Add error message for 1-variable dataframe

--- R/dss_plot_md_pattern.R
+++ R/dss_plot_md_pattern.R
@@ -6,19 +6,25 @@ function(ref, target, type, rotate = TRUE) {
 ### rotate: boolean, passed to md.pattern() for its rotate.names argument
 
     if (is.null(target)) {
-      return()
+        return()
+    } else if (ncol(ref) == 1) {
+        plot(x = 0, y = 0, pch = "",
+             xlim = c(-5, 5), ylim = c(-5, 5))
+        text(x = 0, y = 0,
+             col = "red",
+             labels = "Error: At least two variables are needed for this plot.")
     } else if (type == "pattern") {
-      par(mar = c(1, 1, ifelse(rotate, 0, 1), 1))
-      mice::md.pattern(x = ref[, -1], # without Sex factor
-                       plot = TRUE,
-                       rotate.names = rotate)
+        par(mar = c(1, 1, ifelse(rotate, 0, 1), 1))
+        mice::md.pattern(x = ref[, -1], # without Sex factor
+                         plot = TRUE,
+                         rotate.names = rotate)
     } else if (type == "map") {
-      par(mar = c(1, 1, 1, 1), cex = 1.3)
-      mm <- visdat::vis_miss(ref[, -1]) # without Sex factor
-      mm +
-        theme(legend.text = element_text(size = 12),
-              axis.text.x = element_text(size = 10),
-              axis.text.y = element_text(size = 12),
-              axis.title.y = element_text(size = 12))
+        par(mar = c(1, 1, 1, 1), cex = 1.3)
+        mm <- visdat::vis_miss(ref[, -1]) # without Sex factor
+        mm +
+            theme(legend.text = element_text(size = 12),
+                  axis.text.x = element_text(size = 10),
+                  axis.text.y = element_text(size = 12),
+                  axis.title.y = element_text(size = 12))
     }
 }
