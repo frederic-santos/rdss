b79788528ec6898eddea9a8dcafc913b41932803
f-santos <frederic.santos@u-bordeaux.fr>
2020-11-30 10:02:35 +0100

    Fix: sex also counts as a column here

--- R/dss_impute_missing.R
+++ R/dss_impute_missing.R
@@ -5,8 +5,9 @@ dss_impute_missing <- function(dtf, method = "missMDA") {
     if (total_perc_missing(dtf) == 0) {
         ## No missing values: return dtf itself
         return(dtf)
-    } else if (ncol(dtf) == 1) {
-        ## One single column: casewise deletion
+    } else if (ncol(dtf) == 2) {
+        ## One single metric variable: casewise deletion
+        ## (ncol == 2 because Sex also counts as a column)
         return(na.omit(dtf))
     } else if (method == "missMDA") {
         nb <- missMDA::estim_ncpPCA(dtf[, -1])
