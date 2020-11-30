47ede060ea9780f6df822f0957616b187ead8c87
f-santos <frederic.santos@u-bordeaux.fr>
2020-11-30 09:33:08 +0100

    Handle possible case of 1-variable dataframe

--- R/dss_impute_missing.R
+++ R/dss_impute_missing.R
@@ -1,7 +1,13 @@
-dss_impute_missing <-
-function(dtf, method = "missMDA") {
+dss_impute_missing <- function(dtf, method = "missMDA") {
+### dtf: dataframe
+### method: either "missMDA" or "missForest"
+
     if (total_perc_missing(dtf) == 0) {
+        ## No missing values: return dtf itself
         return(dtf)
+    } else if (ncol(dtf) == 1) {
+        ## One single column: casewise deletion
+        return(na.omit(dtf))
     } else if (method == "missMDA") {
         nb <- missMDA::estim_ncpPCA(dtf[, -1])
         imp <- missMDA::imputePCA(X = dtf[, -1], ncp = nb$ncp)$completeObs
